5d1d6f21d80a6ab4adab2bf487f02d67c7b18453
Ciaran <>
2023-07-19 08:34:02 +0100

    Add jitter.
    The run.sh script is invoked via cron. I want to add a jitter so I am adding
    it in the script rather than touching the crontab file.
    
    The queries to the crt.sh database over SQL have no jitter either so I should
    update that too so their database is not being hammered.
    
    Right now a query per domain in domains.txt is sent so if you have 1000 domains
    in domains.txt the code will try to run 1000 concurrent queries.

--- run.sh
+++ run.sh
@@ -2,10 +2,13 @@
 
 LOCK_FILE=/tmp/running.lock
 SUBLERT=sublert-http
+TIME_TO_SLEEP_IN_SECONDS=300
 
 if [[ -f "$LOCK_FILE" ]]; then
   sleep 1
 else
+  # Using this as a jitter for cron
+  sleep $((RANDOM % TIME_TO_SLEEP_IN_SECONDS))
   touch "$LOCK_FILE"
   cd $HOME/$SUBLERT
   source runtime/bin/activate
